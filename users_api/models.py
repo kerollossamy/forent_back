62d31fd268975e02b2afed93ed89769ebd29f033
kerollossamy <kerollossamy9908@gmail.com>
2024-03-28 17:32:45 +0200

    Fixed image field in User model to upload image

--- users_api/models.py
+++ users_api/models.py
@@ -1,42 +1,25 @@
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
-from django.core.validators import FileExtensionValidator
-from for_rent import settings
 
-def image_upload(instance, filename):
-    imagename, extension = filename.split(".")
-    return f"user/{instance.id}.{extension}"
-
-
-class Role(models.TextChoices):
-    RENTER = "Renter", "Renter"
-    OWNER = "Owner", "Owner"
-    ADMIN = "Admin", "Admin"
 
 
 class User(models.Model):
+    USER_ROLES = [
+        ("owner", "Owner"),
+        ("renter", "Renter"),
+    ]
     username = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=30, blank=True)
-    role = models.CharField(max_length=7, choices=Role.choices, default=Role.RENTER)
+    role = models.CharField(max_length=7, choices=USER_ROLES)
     validation_states = models.BooleanField(default=False)
     registration_date = models.DateTimeField(auto_now=True)
     profile_picture = models.ImageField(
-        upload_to=image_upload,
-        default= settings.MEDIA_URL + settings.DEFAULT_PROFILE_IMAGE,
-        validators=[
-            FileExtensionValidator(allowed_extensions=["png", "jpg", "jpeg"]),
-        ],
-        blank=True, null=True
+        upload_to="static/profile_pictures/",
+        default= "static/images/blank_profile.png"
     )
     phone_number = PhoneNumberField(region="EG", blank=True, null=True)
 
     def __str__(self):
-        return self.name
-
-    def get_profile_picture_url(self):
-        if self.profile_picture:
-            return self.profile_picture.url
-        else:
-            return settings.DEFAULT_PROFILE_IMAGE
+        return self.username
